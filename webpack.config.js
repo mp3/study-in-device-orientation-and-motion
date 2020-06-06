8ca6f282bed770009692108f696aab8c5cba67a2
mp3 <im.in.a.coma.xxx@gmail.com>
2020-06-06 16:46:45 +0900

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".json"],
+    alias: {
+      "react": "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",
+    }
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: path.join(__dirname, "src/index.html")
+    })
+  ]
+}
